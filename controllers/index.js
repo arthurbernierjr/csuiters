3d6defc69281bf6a8b3a3dd198ff1d83fcc7d9a4
Arthur Bernier Jr <ma2bernier@gmail.com>
2024-02-02 13:55:18 -0500

    Changes Noted Below
    env update

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -9,7 +10,7 @@ const Lead = require('../models/lead')
 exports.auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '')
-    const data = jwt.verify(token, 'secret')
+    const data = jwt.verify(token, process.env.SECRET)
     const user = await User.findOne({ _id: data._id })
     if (!user) {
       throw new Error()
